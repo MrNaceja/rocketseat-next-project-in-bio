6353ffcd14fdc139e4692ed69e11ba684f152537
Eduardo Toriani <edutoriani13@gmail.com>
2025-09-30 22:22:30 -0300

    feat: ui components and add button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import { Button } from "@/ui";
+
 export default function HomePage() {
     return (
         <section>
             <h1>Hello World</h1>
+            <Button>
+                Criar agora
+            </Button>
+            <Button variant="secondary">
+                Criar agora
+            </Button>
+            <Button variant="outline">
+                Criar agora
+            </Button>
         </section>
     )
 }
---
+++ src/ui/button.tsx
@@ -0,0 +1,25 @@
+import type { HTMLAttributes } from "react"
+import { tv, type VariantProps } from "tailwind-variants"
+
+const button = tv({
+    base: "rounded-xl p-3 typografy-label text-content-heading border border-transparent transition-colors",
+    variants: {
+        variant: {
+            outline: "border-border-primary hover:bg-background-secondary/50",
+            primary: "bg-accent-purple hover:bg-accent-purple/80",
+            secondary: "bg-background-tertiary hover:bg-background-tertiary/80"
+        }
+    },
+    defaultVariants: {
+        variant: "primary"
+    }
+})
+
+type ButtonVariants = VariantProps<typeof button>
+
+interface ButtonProps extends ButtonVariants, HTMLAttributes<HTMLButtonElement> { }
+export function Button({ className, variant,...props }: ButtonProps) {
+    return (
+        <button className={button({ variant, className })} {...props} />
+    )
+}
