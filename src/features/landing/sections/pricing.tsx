ec6641bcbe13363ebb7faad89bf4bee7a86cbe77
Eduardo Toriani <edutoriani13@gmail.com>
2025-10-02 21:38:51 -0300

    feat: implement answers section on landing

--- src/features/landing/sections/pricing.tsx
+++ src/features/landing/sections/pricing.tsx
@@ -2,7 +2,7 @@ import { PlanCard } from "@features/landing/plan-card"
 
 export function PricingSection() {
     return (
-        <section className="min-h-screen container space-y-6 p-20">
+        <section className="min-h-screen container gap-6 px-20 flex flex-col items-center justify-center">
             <div className="flex flex-col items-center gap-6 text-center">
                 <h2 className="text-content-heading typografy-heading-medium">Um valor acessível para todos</h2>
                 <p className="text-content-body typografy-body-large max-w-[50%]">
